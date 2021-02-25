d2c4cb483832ea61ef9d56e52a5f80272b6b0b68
ajmalbinnizam <ajmalnizam369@gmail.com>
2021-02-26 00:49:54 +0530

    restarted the project

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,74 +1,257 @@
-# Write your code here
-#Design
-"""
-1. Every person can create 1 or more objects
-2. Every object will have all the methods and data relevant for that object.
-3. Data will consist of dict of card numbers (400000 + 9 digit account number + 0)
-and corresponding PINs
-3.a Account number can be derived from the card number
-3.b Every object will have 0 or more card numbers
-4. Every object will start with a dict of 0 cards
-4b. These objects can be modified through the instance methods
-"""
+# # Write your code here
+# #Design
+# """
+# 1. Every person can create 1 or more objects
+# 2. Every object will have all the methods and data relevant for that object.
+# 3. Data will consist of dict of card numbers (400000 + 9 digit account number + 0)
+# and corresponding PINs
+# 3.a Account number can be derived from the card number
+# 3.b Every object will have 0 or more card numbers
+# 4. Every object will start with a dict of 0 cards
+# 4b. These objects can be modified through the instance methods
+# """
+#
+#
+# # Write your code here
+# class BankingAccount(object):
+#     def __init__(self):
+#         self.cardmasterdata = {}
+#         self.accountnumberlist = ["000000000"]
+#         self.cardnumber = None
+#
+#     def createcard(self):
+#         import random
+#         newaccountint = int(max(self.accountnumberlist)) + 1
+#         newaccountchar = f"{newaccountint:09d}"
+#         #Append new account to the master account list
+#         self.accountnumberlist.append(newaccountchar)
+#         #Create the new cardnumber
+#         cardnumber = "400000" + newaccountchar + "0"
+#         pin = str(random.randint(1000, 9999))
+#         #Append new card to the cardmasterdata. Also, map it to its pin
+#         self.cardmasterdata[cardnumber] = pin
+#         return (cardnumber, pin)
+#
+#     def loginaccount(self, cardnumber, pin):
+#         self.cardnumber = cardnumber
+#         if cardnumber in self.cardmasterdata.keys() and self.cardmasterdata[cardnumber] == pin:
+#             print("You have successfully logged in!\n")
+#             while True:
+#                 print("1. Balance\n2. Log out\n0. Exit")
+#                 logininput = input()
+#                 if logininput == "1":
+#                     print("Balance: 0")
+#                 elif logininput == "2":
+#                     print("You have successfully logged out!")
+#                     break
+#                 elif logininput == "0":
+#                     print("Bye")
+#                     exit()
+#         else:
+#             print("Wrong card number or PIN!")
+#
+#
+# ###Actual start of program
+# #Create an object in the beginning. The same object will be used throughout the program
+# BankingAccountObject1 = BankingAccount()
+# while True:
+#     print("1. Create an account \n2. Log into account\n0. Exit")
+#     userinput = input()
+#     if userinput == "1":
+#         currentcardnumber, currentpin = BankingAccountObject1.createcard()
+#         print(f"Your card has been created\nYour card number:\n{currentcardnumber}")
+#         print(f"Your card PIN:\n{currentpin}")
+#     elif userinput == "2":
+#         print("Enter your card number:")
+#         cardnumber = input()
+#         print("Enter your PIN:")
+#         pin = input()
+#         BankingAccountObject1.loginaccount(cardnumber, pin)
+#     elif userinput == "0":
+#         print("Bye!")
+#         break
+#
+# exit()
+
+import random
+from string import digits
+import sqlite3 as sq
 
 
-# Write your code here
-class BankingAccount(object):
-    def __init__(self):
-        self.cardmasterdata = {}
-        self.accountnumberlist = ["000000000"]
-        self.cardnumber = None
+class BankSystem:
+    prompt1 = '1. Create an account\n2. Log into account\n0. Exit\n'
+    prompt2 = '1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit\n'
+    prompt3 = 'You have successfully logged in!\n'
+    prompt4 = 'Wrong card number or PIN!\n'
+    prompt5 = 'Enter your card number:\n'
+    prompt6 = 'Enter your PIN:\n'
+    prompt7 = 'Your card number:>'
+    prompt8 = 'Your card PIN:'
+    prompt9 = 'Your card has been created'
+    prompt10 = 'Wrong choice'
+    prompt11 = 'You have successfully logged out!\n'
+    prompt12 = 'Enter income:\n'
+    prompt13 = 'Income was added!\n'
+    prompt14 = 'The account has been closed!\n'
+    IIN = '400000'
+    logged_card = None
 
-    def createcard(self):
-        import random
-        newaccountint = int(max(self.accountnumberlist)) + 1
-        newaccountchar = f"{newaccountint:09d}"
-        #Append new account to the master account list
-        self.accountnumberlist.append(newaccountchar)
-        #Create the new cardnumber
-        cardnumber = "400000" + newaccountchar + "0"
-        pin = str(random.randint(1000, 9999))
-        #Append new card to the cardmasterdata. Also, map it to its pin
-        self.cardmasterdata[cardnumber] = pin
-        return (cardnumber, pin)
+    @staticmethod
+    def input(prompt):
+        action = input(prompt)
+        return action
 
-    def loginaccount(self, cardnumber, pin):
-        self.cardnumber = cardnumber
-        if cardnumber in self.cardmasterdata.keys() and self.cardmasterdata[cardnumber] == pin:
-            print("You have successfully logged in!\n")
-            while True:
-                print("1. Balance\n2. Log out\n0. Exit")
-                logininput = input()
-                if logininput == "1":
-                    print("Balance: 0")
-                elif logininput == "2":
-                    print("You have successfully logged out!")
-                    break
-                elif logininput == "0":
-                    print("Bye")
-                    exit()
+    @staticmethod
+    def checksum_lhun(string):
+        digit = list(map(int, string))
+        odd_sum = sum(digit[-1::-2])
+        even_sum = sum([sum(divmod(2 * d, 10)) for d in digit[-2::-2]])
+        return (odd_sum + even_sum) % 10
+
+    def verify_card(self, number):
+        return self.checksum_lhun(number) == 0
+
+    def generate_lhun(self, string):
+        #  Generate the Luhn check digit to append to the provided string.
+        cksum = self.checksum_lhun(string + '0')
+        return str((10 - cksum) % 10)
+
+    def append_lhun(self, string):
+        #  Append Luhn check digit to the end of the provided string
+        return string + str(self.generate_lhun(string))
+
+    def main_menu(self):
+        while True:
+            action = input(self.prompt1)
+            if action == '1':
+                self.create_card()
+            elif action == '2':
+                self.login()
+            elif action == '0':
+                print('Bye!')
+                conn.close()
+                exit()
+
+    def create_card(self):
+        card_number = self.generate_number()
+        card_pin = self.generate_pin()
+        user_id = random.randint(0, 1000000)
+        print('{}\n{}\n{}\n{}\n{}'.format(self.prompt9, self.prompt7, card_number, self.prompt8, card_pin))
+        cur.execute("""INSERT INTO card VALUES (?, ?, ?, ?)""", (user_id, card_number, card_pin, 0))
+        conn.commit()
+
+    def generate_number(self):
+        number = self.IIN
+        for i in range(9):
+            number += random.choice(digits)
+        number += self.generate_lhun(number)
+        return number
+
+    @staticmethod
+    def generate_pin():
+        pin = ''
+        for i in range(4):
+            pin += random.choice(digits)
+        return pin
+
+    def login(self):
+        card_number = self.input(self.prompt5)
+        card_pin = self.input(self.prompt6)
+        cur.execute("""SELECT number, pin FROM card WHERE number=:number""", {"number": card_number})
+        if (card_number, card_pin) == cur.fetchone():
+            self.logged_card = card_number
+            print(self.prompt3)
+            self.account_action()
         else:
-            print("Wrong card number or PIN!")
+            print(self.prompt4)
+
+    def balance(self):
+        cur.execute("""SELECT balance FROM card WHERE number=:number""", {"number": self.logged_card})
+        balance = cur.fetchone()
+        print(f'Balance: ', balance[0])
+
+    def add_income(self):
+        income = int(self.input(self.prompt12))
+        cur.execute("""UPDATE card SET balance = balance +? WHERE number=?""", (income, self.logged_card))
+        conn.commit()
+        print(self.prompt13)
+
+    def account_action(self):
+        while True:
+            print(self.prompt2)
+            action = input()
+            if action == '1':
+                self.balance()
+            elif action == '2':
+                self.add_income()
+            elif action == '3':
+                self.transfer_main()
+            elif action == '4':
+                self.close_account()
+                break
+            elif action == '5':
+                self.logged_card = None
+                print(self.prompt11)
+                break
+            elif action == '0':
+                print('\nBye!')
+                conn.close()
+                exit()
+
+    def close_account(self):
+        cur.execute("""DELETE FROM card 
+                        WHERE number=:number""", {"number": self.logged_card})
+        conn.commit()
+        self.logged_card = None
+        print(self.prompt14)
+
+    def transfer_main(self):
+        print('Transfer')
+        card_input = input('Enter card number:\n')
+        if self.check_transfer(card_input) is True:
+            amount = int(input('Enter how much money you want to transfer:\n'))
+            if self.check_transfer_balance(amount) is True:
+                self.make_transaction(card_input, amount)
+
+    def check_transfer(self, number):
+        if self.verify_card(number) is True:
+            if self.check_card_in_db(number) is True:
+                return True
+            else:
+                print('Such a card does not exist.\n')
+                return False
+        else:
+            print('Probably you made mistake in the card number. Please try again!\n')
+            return False
+
+    def check_transfer_balance(self, amount):
+        cur.execute("""SELECT balance FROM card WHERE number=:number""", {"number": self.logged_card})
+        balance = cur.fetchone()
+        if amount <= balance[0]:
+            return True
+        else:
+            print('Not enough money!\n')
+
+    @staticmethod
+    def check_card_in_db(number):
+        cur.execute("""SELECT number FROM card WHERE number=:number""", {"number": number})
+        if cur.fetchone() is not None:
+            return True
+
+    def make_transaction(self, number_to, amount):
+        cur.execute("""UPDATE card SET balance = balance +? WHERE number=?""", (amount, number_to))
+        cur.execute("""UPDATE card SET balance = balance -? WHERE number=?""", (amount, self.logged_card))
+        conn.commit()
+        print('Success!\n')
 
 
-###Actual start of program
-#Create an object in the beginning. The same object will be used throughout the program
-BankingAccountObject1 = BankingAccount()
-while True:
-    print("1. Create an account \n2. Log into account\n0. Exit")
-    userinput = input()
-    if userinput == "1":
-        currentcardnumber, currentpin = BankingAccountObject1.createcard()
-        print(f"Your card has been created\nYour card number:\n{currentcardnumber}")
-        print(f"Your card PIN:\n{currentpin}")
-    elif userinput == "2":
-        print("Enter your card number:")
-        cardnumber = input()
-        print("Enter your PIN:")
-        pin = input()
-        BankingAccountObject1.loginaccount(cardnumber, pin)
-    elif userinput == "0":
-        print("Bye!")
-        break
-
-exit()
+conn = sq.connect('card.s3db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS card 
+                (
+                id INTEGER,
+                number TEXT,
+                pin TEXT,
+                balance INTEGER DEFAULT 0
+                )""")
+BankSystem().main_menu()
--- Simple Banking System/task/tests.py
+++ Simple Banking System/task/tests.py
@@ -1,16 +1,22 @@
-from hstest.exceptions import WrongAnswer
 from hstest.test_case import CheckResult
-from hstest.stage_test import StageTest
+from hstest.stage_test import StageTest, WrongAnswer
 from hstest.test_case import TestCase
+from shutil import copy2
+import sqlite3
 import random
 import re
+import os
 
 card_number = ''
 pin = ''
+second_card_number = ''
+second_pin = ''
 are_all_inputs_read = False
+db_file_name = 'card.s3db'
+temp_db_file_name = 'temp.s3db'
 
 
-def get_credentials(output: str):
+def get_credentials(output: str, count=0):
     number = re.findall(r'^400000\d{10}$', output, re.MULTILINE)
     if not number:
         raise WrongAnswer('You are printing the card number incorrectly. '
@@ -24,8 +30,12 @@ def get_credentials(output: str):
                                    'The PIN should look like in the example: DDDD, where D is a digit.\n'
                                    'Make sure the PIN is 4-digit length and you don\'t print any extra spaces at the'
                                    ' end of the line!')
-
-    return number[0], PIN[0]
+    if count == 2:
+        if len(number) == 1 or len(PIN) == 1:
+            raise WrongAnswer(f'Expected 2 generated card credentials, but found {len(number)}')
+        return (number[0], PIN[0]), (number[1], PIN[1])
+    else:
+        return number[0], PIN[0]
 
 
 def test_card_generation(output: str, value_to_return):
@@ -130,16 +140,214 @@ def test_luhn_algorithm(output: str, correct_num_of_cards):
     return '0'
 
 
+def check_db(output: str, value_to_return):
+    if not os.path.exists(db_file_name):
+        return CheckResult.wrong('Can\'t find db file named \'{}\''.format(db_file_name))
+    try:
+        copy2(db_file_name, temp_db_file_name)
+    except Exception:
+        return CheckResult.wrong('Can\'t copy database file!')
+
+    try:
+        with sqlite3.connect(db_file_name) as db:
+            response = db.execute(
+                'SELECT name FROM sqlite_master WHERE type = \'table\' AND name NOT LIKE \'sqlite_%\';')
+            for _ in response.fetchall():
+                if 'card' in _:
+                    break
+            else:
+                return CheckResult.wrong('Your database doesn\'t have a table named \'card\'')
+    except Exception as exp:
+        return CheckResult.wrong('Can\'t connect to the database!')
+
+    correct_columns = (('ID', 'INTEGER'), ('NUMBER', 'TEXT'), ('PIN', 'TEXT'), ('BALANCE', 'INTEGER'))
+
+    try:
+        with sqlite3.connect(db_file_name) as db:
+            response = db.execute('PRAGMA table_info(card);')
+            real_columns = response.fetchall()
+            for correct_column in correct_columns:
+                for real_column in real_columns:
+                    real_column = [str(element).upper() for element in real_column]
+                    if correct_column[0] in real_column and correct_column[1] in real_column:
+                        break
+                else:
+                    return CheckResult.wrong(
+                        f'Can\'t find column named \'{correct_column[0].lower()}\' with \'{correct_column[1]}\' type.\n'
+                        'Your table should have columns described in the stage instructions.')
+    except Exception:
+        return CheckResult.wrong('Can\'t connect to the database!')
+
+    return CheckResult.correct()
+
+
+def check_db_rows(output: str, value_to_return):
+    correct_num_of_cards = 10
+    numbers = re.findall(r'400000\d{10,}', output, re.MULTILINE)
+
+    for number in numbers:
+        if len(number) != 16:
+            return CheckResult.wrong(f'Wrong card number \'{number}\'. The card number should be 16-digit length.')
+        if not is_passed_luhn_algorithm(number):
+            return CheckResult.wrong('The card number \'{}\' doesn\'t pass luhn algorithm!'.format(number))
+
+    if len(numbers) != correct_num_of_cards:
+        return CheckResult.wrong(
+            f'After creating {correct_num_of_cards} cards, found {len(numbers)} cards with correct format\n'
+            f'The card number should be 16-digit length and should start with 400000.')
+
+    with sqlite3.connect(db_file_name) as db:
+        rows = db.execute('SELECT * FROM card').fetchall()
+        for number in numbers:
+            if len(number) != 16:
+                return CheckResult.wrong(f'Wrong card number \'{number}\'. The card number should be 16-digit length.')
+            is_found = False
+            for row in rows:
+                if number in row:
+                    is_found = True
+            if not is_found:
+                return CheckResult.wrong('Your database doesn’t save newly created cards.\n'
+                                         'Make sure you commit your DB changes right after saving a new card in the database!')
+    return CheckResult.correct()
+
+
+def test_add_income(output: str, value_to_return):
+    global card_number, pin, are_all_inputs_read
+    are_all_inputs_read = False
+    credentials = get_credentials(output)
+    card_number = credentials[0]
+    pin = credentials[1]
+    return '2\n{}\n{}\n2\n10000'.format(card_number, pin)
+
+
+def test_second_add_income(output: str, value_to_return):
+    global card_number
+    expected_balance = 10000
+    with sqlite3.connect(db_file_name) as db:
+        result = db.execute('SELECT * FROM card WHERE number = {}'.format(card_number)).fetchone()
+        if not result:
+            return CheckResult.wrong(f'Can\' find card number \'{card_number}\' in the database!\n'
+                                     f'Make sure you commit your DB changes right after saving a new card in the database!')
+        balance = result[3]
+        if balance != expected_balance:
+            return CheckResult.wrong(
+                'Account balance is wrong after adding income. Expected {}'.format(expected_balance))
+    return '2\n15000'
+
+
+def test_balance_after_second_income(output: str, value_to_return):
+    global card_number, are_all_inputs_read
+    expected_balance = 25000
+    with sqlite3.connect(db_file_name) as db:
+        result = db.execute('SELECT * FROM card WHERE number = {}'.format(card_number)).fetchone()
+        if not result:
+            return CheckResult.wrong(f'Can\' find card number \'{card_number}\' in the database!\n'
+                                     f'Make sure you commit your DB changes right after saving a new card in the database!')
+        balance = result[3]
+        if balance != expected_balance:
+            return CheckResult.wrong(
+                'Account balance is wrong after adding income. Expected {}'.format(expected_balance))
+    are_all_inputs_read = True
+    return value_to_return
+
+
+def test_transfer(output: str, value_to_return):
+    global card_number, pin, second_card_number, second_pin, are_all_inputs_read
+    are_all_inputs_read = False
+    credentials = get_credentials(output, count=2)
+    card_number, pin = credentials[0]
+    second_card_number, second_pin = credentials[1]
+    doesnt_pass_luhn = 4000003972196502
+    return '2\n{}\n{}\n3\n{}'.format(card_number, pin, doesnt_pass_luhn)
+
+
+def test_transfer_doesnt_pass_luhn(output: str, value_to_return):
+    if 'mistake'.lower() not in output.lower():
+        return CheckResult.wrong('You should not allow to transfer to a card number that doesn\'t pass '
+                                 'the Luhn algorithm.\n Instead output \'{}\''.format(
+            'Probably you made mistake in card number. Please try again!'))
+    doesnt_exist_card = 3000003972196503
+    return '3\n{}'.format(doesnt_exist_card)
+
+
+def test_transfer_doesnt_exist_card(output: str, value_to_return):
+    global second_card_number
+    if 'not exist' not in output.lower():
+        return CheckResult.wrong('You should not allow to transfer to a card number that does not exist.'
+                                 '\nYpu should print \'{}\''.format('Such a card does not exist.'))
+    return '3\n{}\n10000'.format(second_card_number)
+
+
+def test_transfer_not_enough_money(output: str, value_to_return):
+    global second_card_number
+    if 'not enough money' not in output.lower():
+        return CheckResult.wrong('You should not allow a transfer if there is not enough money '
+                                 'in the account to complete it.\n')
+    return '2\n20000\n3\n{}\n10000'.format(second_card_number)
+
+
+def test_balance_after_transfer(output: str, value_to_return):
+    global card_number, second_card_number, are_all_inputs_read
+    with sqlite3.connect(db_file_name) as db:
+        first = db.execute('SELECT * FROM card WHERE number = {}'.format(card_number)).fetchone()
+        if not first:
+            return CheckResult.wrong(f'Can\' find card number \'{first}\' in the database!\n'
+                                     f'Make sure you commit your DB changes right after saving a new card in the database!')
+        second = db.execute('SELECT * FROM card WHERE number = {}'.format(second_card_number)).fetchone()
+        if not second:
+            return CheckResult.wrong(f'Can\' find card number \'{second}\' in the database!\n'
+                                     f'Make sure you commit your DB changes right after saving a new card in the database!')
+        first_balance = first[3]
+        second_balance = second[3]
+        if first_balance != 10000:
+            return CheckResult.wrong('Incorrect account balance of the card used to make the transfer.')
+        if second_balance != 10000:
+            return CheckResult.wrong('Incorrect account balance of the card to which the transfer was made.')
+    are_all_inputs_read = True
+    return '0'
+
+
+def test_closing_account(output: str, value_to_return):
+    global card_number, pin, are_all_inputs_read
+    are_all_inputs_read = False
+    credentials = get_credentials(output)
+    card_number, pin = credentials[0], credentials[1]
+    return '2\n{}\n{}\n4'.format(card_number, pin)
+
+
+def test_rows_after_closing_account(output: str, value_to_return):
+    global card_number, are_all_inputs_read
+    with sqlite3.connect(db_file_name) as db:
+        rows = db.execute('SELECT * FROM card WHERE number = \'{}\''.format(card_number)).fetchall()
+        if rows:
+            return CheckResult.wrong('After closing an account, the card number should be deleted from the database.')
+    are_all_inputs_read = True
+    return value_to_return
+
+
 class BankingSystem(StageTest):
 
     def generate(self):
         return [
+            TestCase(
+                stdin='0',
+                check_function=check_db,
+            ),
             TestCase(
                 stdin=[
                     '1',
                     lambda output: test_card_generation(output, '1'),
                     lambda output: test_difference_between_generations(output, '0')
                 ]),
+            TestCase(
+                stdin=[
+                    '1\n1\n1\n1\n1\n1\n1\n1',
+                    lambda output: test_luhn_algorithm(output, 8),
+                ]),
+            TestCase(
+                stdin='1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n0',
+                check_function=check_db_rows
+            ),
             TestCase(
                 stdin=[
                     '1',
@@ -163,9 +371,27 @@ class BankingSystem(StageTest):
                 ]),
             TestCase(
                 stdin=[
-                    '1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1',
-                    lambda output: test_luhn_algorithm(output, 11),
-                ])
+                    '1',
+                    lambda output: test_add_income(output, None),
+                    lambda output: test_second_add_income(output, None),
+                    lambda output: test_balance_after_second_income(output, '0'),
+                ]),
+            TestCase(
+                stdin=[
+                    '1\n1',
+                    lambda output: test_transfer(output, None),
+                    lambda output: test_transfer_doesnt_pass_luhn(output, None),
+                    lambda output: test_transfer_doesnt_exist_card(output, None),
+                    lambda output: test_transfer_not_enough_money(output, None),
+                    lambda output: test_balance_after_transfer(output, None),
+                ]),
+            TestCase(
+                stdin=[
+                    '1',
+                    lambda output: test_closing_account(output, None),
+                    lambda output: test_rows_after_closing_account(output, '0')
+                ]
+            )
         ]
 
     def check(self, reply: str, attach) -> CheckResult:
@@ -173,6 +399,11 @@ class BankingSystem(StageTest):
             return CheckResult.correct()
         else:
             return CheckResult.wrong('You didn\'t read all inputs!')
+
+    def after_all_tests(self):
+        if os.path.exists('temp.s3db'):
+            copy2('temp.s3db', 'card.s3db')
+            os.remove('temp.s3db')
 
 
 if __name__ == '__main__':
